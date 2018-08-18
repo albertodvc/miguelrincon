4e230e916c27062db06438d9e4b40b10d1de4ec5
Alberto del Valle <alberto.dvc@gmail.com>
2018-08-18 19:30:10 +0200

    Front Page video (issue #3)

--- components/FrontPage.vue
+++ components/FrontPage.vue
@@ -6,6 +6,133 @@
 
 </template>
 
+<style lang="scss">
+
+@import "compass/css3";
+@import "compass/utilities";
+@import "variables";
+@import "typography";
+@import "media_queries";
+
+#frontPage {
+
+  @include breakpoint(690px) {
+    h2 {
+      bottom: 92px;
+    }
+  }
+
+	header,
+	&.parallax-container {
+		background-image: url('~/assets/images/sections/guitarra_1.jpg');
+		&:before {
+			content: "";
+			@include box-sizing(border-box);
+			display: block;
+			background: inline-image('~/assets/images/sections/guitarra_1_load.jpg');
+			position: absolute;
+			top: 0;
+			width: 100%;
+			left: 0;
+			background-repeat: no-repeat;
+    		background-position: center center;
+    		background-size: cover;
+    		z-index: 0;
+    		@include filter(blur(5px));
+    		@include transition(opacity 0.5s ease-in-out);
+    		opacity: 1;
+		}
+		&.img-loaded:before {
+			opacity: 0;
+		}
+	}
+	&.parallax-container {
+		&:before {
+			position: absolute;
+			height: 100%;
+			background-repeat: no-repeat;
+			background-attachment: fixed;
+    		background-position: center center;
+    		background-size: cover;
+		}
+	}
+
+	header,
+	header:before,
+	&.parallax-container:before {
+		margin: 0;
+		padding: 0;
+		height: $smal-section-header-height + $top-bar-height;
+		padding-top: $top-bar-height;
+
+		@include breakpoint(690px) {
+			height: $medium-section-header-height + $top-bar-height;
+		}
+		@include breakpoint(958px) {
+			height: 500px + $top-bar-height;
+		}
+		@include breakpoint(1025px) {
+			height:100vh;
+		}
+	}
+	&.parallax-container {
+		header {
+			background: none;
+			padding: 0;
+			.video-wrapper {
+				position: fixed;
+			}
+			&:before {
+				content: none;
+				display: none;
+				opacity: 0;
+			}
+		}
+		@include breakpoint(1024px) {
+			h2 { position: fixed;}
+		}
+	}
+
+	h2 {
+		width: 100%;
+		text-align: center;
+		@include breakpoint(1024px) {
+			width: 960px;
+		}
+		span {
+			display: none;
+			@include breakpoint(958px) {
+				display: block;
+			}
+		}
+	}
+	.content {
+		p {
+
+ 			&:first-letter {
+ 				font-family: "main-header";
+ 				font-size: 130px;
+ 				line-height: 124px;
+ 				margin-bottom: -25px;
+ 				padding-right: 15px;
+ 				font-weight: 800;
+ 				color: $black;
+ 				float: left;
+ 				display: block;
+ 			}
+ 			@include breakpoint(950px) {
+ 				@include column-count(2);
+				@include column-gap(40px);
+ 				text-align: justify;
+ 			}
+		}
+	}
+
+}
+
+
+</style>
+
 <script>
 
   import VueMarkdown from 'vue-markdown'
--- components/VideoPlayer.vue
+++ components/VideoPlayer.vue
@@ -1,13 +1,12 @@
 <template>
-  <div>
+  <div class="video-player"
+    :class="{'video-ready': ready, 'video-active': playing, 'played-once': playedOnce}">
     <h2 v-parallax>
       <slot></slot>
-      <div class="button-container">
+      <div class="button-container" @click.native="playVideo">
         <button class="play-video-button"
-          role="button"
           :class="{ playing: playing }"
-
-          @click.native="playVideo">
+          @click="playVideo">
           <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="icon-play" x="0px" y="0px" viewBox="0 0 512 512" xml:space="preserve">
             <path class="icon" d="M204.8,371.2L358.4,256L204.8,140.8V371.2z M256,0C114.562,0,0,114.562,0,256s114.562,256,256,256s256-114.562,256-256  S397.438,0,256,0z M256,460.8C143.106,460.8,51.2,368.9,51.2,256c0-112.894,91.906-204.8,204.8-204.8  c112.9,0,204.8,91.906,204.8,204.8C460.8,368.9,368.9,460.8,256,460.8z" fill="#FFFFFF"/>
           </svg>
@@ -15,19 +14,93 @@
         </button>
       </div>
     </h2>
-    <div class="video-wrapper">
-      <youtube
-        :video-id="video.id"
-        :player-vars="playerVars"
-        ref="youtube"
-        width="100%"
-        height="100%"
-        @ready="ready = true"
-        @ended="playing = false; initPlaying = false;"
-        @playing="initPlaying = true"/>
+    <div class="video-wrapper" :class="{'active': playing}">
+      <div class="video-container">
+        <youtube
+          :video-id="video.id"
+          :player-vars="playerVars"
+          ref="youtube"
+          width="100%"
+          height="100%"
+          @ready="ready = true"
+          @ended="playing = false"
+          @paused="playing = false; paused = true"
+          @playing="playing = true; playedOnce = true"/>
+      </div>
     </div>
   </div>
 </template>
+
+<style lang="scss">
+
+@import "compass/css3";
+@import "compass/utilities";
+@import "variables";
+@import "typography";
+@import "media_queries";
+
+.video-player {
+  .video-wrapper {
+    position: absolute;
+    z-index: 1;
+    width: 100%;
+    height: calc(100% - #{$top-bar-height});
+    .video-container {
+      @include transition(opacity 1s ease-in-out);
+      width: 100%;
+      height: 100%;
+      opacity: 0;
+    }
+	}
+  .play-video-button {
+    opacity: 0;
+    background: none;
+    outline: none;
+    border: none;
+    margin: 0;
+    padding: 0;
+    width: 105px;
+    @include transition(all 0.3s ease-in-out);
+    span {
+      font-size: 16px;
+      color: white;
+      display: block;
+    }
+  }
+  &.video-ready {
+    .play-video-button {
+      @include transform(scale(1));
+      @include transition(opacity 0.3s ease-in-out);
+      opacity: 1;
+    }
+  }
+
+  &.video-active {
+    .video-wrapper {
+      z-index: 1!important;
+      .video-container {
+        @include transition(opacity 1s ease-in-out);
+        opacity: 1;
+        pointer-events: auto!important;
+
+      }
+    }
+    .play-video-button {
+      @include transition(all 0.15s ease-in-out);
+      opacity: 0;
+      @include transform(scale(20));
+    }
+  }
+	.video-wrapper:not(.video-active) {
+		height: 100%;
+		z-index: -1;
+		.video-container {
+			pointer-events: none;
+		}
+	}
+}
+
+</style>
 
 <script>
 
@@ -60,7 +133,7 @@
         },
         ready: false,
         playing: false,
-        initPlaying: false,
+        playedOnce: false,
         paused: false,
         ended: true
       }
