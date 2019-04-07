25652f75668006970cf007c2cb7fc0def4157d85
Alberto del Valle <alberto.dvc@gmail.com>
2019-04-07 21:48:23 +0200

    Disables parallax on mobile

--- plugins/parallax.js
+++ plugins/parallax.js
@@ -19,63 +19,66 @@ function findAncestor (el, cls) {
 const parallax = {
   // directive definition
   inserted: function (el) {
-    var container = findAncestor(el, 'parallax-container')
-    //Todo don't do this here
-    if (container)
-      var video = container.querySelector('.video-wrapper')
+    const touch = 'ontouchstart' in document.documentElement
+    if ( !touch && ViewPort.width() > 1024) {
+      var container = findAncestor(el, 'parallax-container')
+      //Todo don't do this here
+      if (container)
+        var video = container.querySelector('.video-wrapper')
 
-    if (!container) return
-    var center = () => {
-      var setCssProperties = (element, opacity, translate) => {
-        var styles = {}
-        if (typeof opacity != 'undefined' && opacity != null) {
-          styles['opacity'] = opacity
+      if (!container) return
+      var center = () => {
+        var setCssProperties = (element, opacity, translate) => {
+          var styles = {}
+          if (typeof opacity != 'undefined' && opacity != null) {
+            styles['opacity'] = opacity
+          }
+          if (typeof translate != 'undefined' && translate != null) {
+            var value = 'translate3d(0px, ' + translate + 'px, 0px)'
+            // styles['-ms-transform'] = value
+            // styles['-webkit-transform'] = value
+            styles['transform'] = value
+          }
+          Object.keys(styles).forEach( style => element.style[style] = styles[style])
         }
-        if (typeof translate != 'undefined' && translate != null) {
-          var value = 'translate3d(0px, ' + translate + 'px, 0px)'
-          // styles['-ms-transform'] = value
-          // styles['-webkit-transform'] = value
-          styles['transform'] = value
+
+        var viewportHeight = ViewPort.height()
+        var scrollPosition = window.scrollY
+        var offset = container.offsetTop - scrollPosition
+        var titleStyle = window.getComputedStyle(el)
+        var margin = titleStyle.getPropertyValue('margin-top')
+        var parallaxRatio = 1.8
+
+
+
+        if (scrollPosition <= viewportHeight) {
+          if (titleStyle.getPropertyValue('opacity') <= 0) {
+            el.classList.add('hidden')
+          } else {
+            el.classList.remove('hidden')
+          }
+          setCssProperties(el, 1 - (scrollPosition / viewportHeight * 1.2), -scrollPosition / parallaxRatio)
+          //Todo don't do this here
+          if (video && video.classList.contains('active')) {
+            setCssProperties(video, 1 - (scrollPosition / viewportHeight * 1.2))
+          }
+        } else {
+          if (titleStyle.getPropertyValue('opacity') <= 0) {
+            el.classList.add('hidden')
+          } else {
+            el.classList.remove('hidden')
+          }
+
+          if (-80 - (offset / parallaxRatio) >= -80) {
+            setCssProperties(el, 1 + offset / viewportHeight * 1.2, -offset / parallaxRatio)
+          } else {
+            setCssProperties(el, 1, 0)
+          }
         }
-        Object.keys(styles).forEach( style => element.style[style] = styles[style])
       }
-
-      var viewportHeight = ViewPort.height()
-      var scrollPosition = window.scrollY
-      var offset = container.offsetTop - scrollPosition
-      var titleStyle = window.getComputedStyle(el)
-      var margin = titleStyle.getPropertyValue('margin-top')
-      var parallaxRatio = 1.8
-
-
-
-      if (scrollPosition <= viewportHeight) {
-        if (titleStyle.getPropertyValue('opacity') <= 0) {
-          el.classList.add('hidden')
-        } else {
-          el.classList.remove('hidden')
-        }
-        setCssProperties(el, 1 - (scrollPosition / viewportHeight * 1.2), -scrollPosition / parallaxRatio)
-        //Todo don't do this here
-        if (video && video.classList.contains('active')) {
-          setCssProperties(video, 1 - (scrollPosition / viewportHeight * 1.2))
-        }
-      } else {
-        if (titleStyle.getPropertyValue('opacity') <= 0) {
-          el.classList.add('hidden')
-        } else {
-          el.classList.remove('hidden')
-        }
-
-        if (-80 - (offset / parallaxRatio) >= -80) {
-          setCssProperties(el, 1 + offset / viewportHeight * 1.2, -offset / parallaxRatio)
-        } else {
-          setCssProperties(el, 1, 0)
-        }
-      }
+      center()
+      window.addEventListener("scroll", throttle(10, ()=> window.requestAnimationFrame(center)))
     }
-    center()
-    window.addEventListener("scroll", throttle(10, ()=> window.requestAnimationFrame(center)))
   }
 }
 
